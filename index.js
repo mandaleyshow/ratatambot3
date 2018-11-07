4990eed2d28856ebcf3d5fdffc912968fd76175d
mandaleyshow <david.noeldner@icloud.com>
2018-11-07 18:30:35 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,3 +5,5 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(3000, () => console.log(‘Webhook server is listening, port 3000’));
