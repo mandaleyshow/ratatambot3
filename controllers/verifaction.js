ea2a67d437d95f7eb2f29543c4e09bf8ffb4494a
mandaleyshow <david.noeldner@icloud.com>
2018-11-07 18:46:04 +0100

    Update verifaction.js

--- controllers/verifaction.js
+++ controllers/verifaction.js
@@ -1,8 +1,8 @@
 module.exports = (req, res) => {
- const hubChallenge = req.query[‘hub.challenge’];
+ const hubChallenge = req.query['hub.challenge'];
 
-const hubMode = req.query[‘hub.mode’];
- const verifyTokenMatches = (req.query[‘hub.verify_token’] === 
+const hubMode = req.query['hub.mode'];
+ const verifyTokenMatches = (req.query['hub.verify_token'] === 
 ‘redhead’);
 
 if (hubMode && verifyTokenMatches) {
