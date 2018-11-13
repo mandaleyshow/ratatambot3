1da96106cf62687469c264a5c8b9cf1c4ff284e6
mandaleyshow <david.noeldner@icloud.com>
2018-11-13 16:03:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('*', (request, response) => {
-	response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+	response.sendFile(path.join(__dirname, 'client/build', '/index.html'));
 });
 
 if (process.env.NODE_ENV === 'production') {
@@ -15,3 +15,4 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 app.listen(process.env.PORT || 3000, () => console.log('Webhook server is listening, port 3000'));
+
