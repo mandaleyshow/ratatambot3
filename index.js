f1c0b1768e7c5767ef3a604cef1b257e64c9fd29
mandaleyshow <david.noeldner@icloud.com>
2018-11-13 16:06:03 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('*', (request, response) => {
-	response.sendFile(path.join(__dirname, 'client/build', '/index.html'));
+	response.sendFile(path.join(__dirname, 'client/build', 'index.js'));
 });
 
 if (process.env.NODE_ENV === 'production') {
