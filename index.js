712fd192d27aee2fd331f7408b2f3967d7d6fc7a
mandaleyshow <david.noeldner@icloud.com>
2018-11-16 01:56:03 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const path = require('path');
 const app = express();
 
 var url = require('url')
@@ -8,12 +8,12 @@ var url = require('url')
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('*', (request, response) => {
+app.get('/', (request, response) => {
 	response.sendFile(path.join(__dirname, 'client/build', 'index.js'));
 });
 
 if (process.env.NODE_ENV === 'production') {
-	app.use(express.static('client/build'));
+	app.use(express.static(path.join(__dirname, 'client/build')));
 }
 
 app.listen(process.env.PORT || 3000, () => console.log('Webhook server is listening, port 3000'));
